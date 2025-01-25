ad9a14b3072e20da2641bd1204e60be90c1e9681
Тимофей Ласкин <tyuiu.laskinta@outlook.com>
2025-01-25 12:57:17 +0500

    Add project files.

---
+++ Tyuiu.LaskinTA.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LaskinTA.Sprint1.Task0.V0.Lib;
+namespace Tyuiu.LaskinTA.Sprint1.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Ласкин Т.А. | РППБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Ласкин Т.А. | РППБ-24-1                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 9*(6/2*3)/9/3 и печатает*");
+            Console.WriteLine("* результат на экране.                                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:  9 * (6 / 2 * 3) / 9 / 3                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.ReadKey();
+
+        }
+    }
+}
